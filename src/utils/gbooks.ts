37c1b6f5ef8ff4250bb5778bfed0e35103b827e3
Sahil Makhijani <sahil.makhijani@gmail.com>
2021-12-11 19:22:21 +0530

    Added google books api utils

---
+++ src/utils/gbooks.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+import { Book } from './../models/Book';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://www.googleapis.com/books/v1/',
+    timeout: 10000,
+    headers: undefined
+});
+
+export const search = async (query: string) => {
+    const res = await axiosInstance.get<{ items: Book[] }>("volumes", {
+        params: {
+            q: query
+        }
+    });
+
+    return res.data.items;
+}
+
+export const getBook = async (id: string) => {
+    const res = await axiosInstance.get<Book>(`volumes/${id}`);
+
+    return res.data;
+}
